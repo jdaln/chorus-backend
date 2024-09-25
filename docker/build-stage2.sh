19097bac48a41e067236102c086d39e17126401f
Sami <sami.perrin@chuv.ch>
2024-09-25 19:08:53 +0200

    typo

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -4,7 +4,7 @@ set -e
 
 docker build --pull -f dockerfiles/stage2.dockerfile -t registry.dip-dev.thehip.app/chorus-cicd-chorus:${IMAGE_TAG} ..
 docker push registry.dip-dev.thehip.app/chorus-cicd-chorus:${IMAGE_TAG}
-if [[ "$$BRANCH_NAME" == "master" ]]; then
+if [[ "$BRANCH_NAME" == "master" ]]; then
     docker tag registry.dip-dev.thehip.app/chorus-cicd-chorus:${IMAGE_TAG} registry.dip-dev.thehip.app/chorus-cicd-chorus:master
     docker push registry.dip-dev.thehip.app/chorus-cicd-chorus:master
 fi
